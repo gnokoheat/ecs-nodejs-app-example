754aa3a691ae780f77affb56b68ab557b132b8ea
linuxadvise <67831141+linuxadvise@users.noreply.github.com>
2020-10-19 21:58:02 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ success: true, data: 'healthy!' });
+    res.status(200).json({ success: true, data: 'Hello World!' });
 });
 
 module.exports = router;
